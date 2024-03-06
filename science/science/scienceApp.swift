4060a3d9064d7ba50cf8bdae83e3a9d924c8dcca
Orcani <140945075+Orcani@users.noreply.github.com>
2024-03-06 14:03:20 +0100

    Premier ajout de Del

---
+++ science/science/scienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  scienceApp.swift
+//  science
+//
+//  Created by Apprenant 38 on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct scienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
