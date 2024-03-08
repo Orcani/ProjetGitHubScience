a5c62c3f558c6101a0cd54ee7b97d1fb75b6a62f
machiavelli913 <162561810+machiavelli913@users.noreply.github.com>
2024-03-08 09:37:30 +0100

    kenza modif

---
+++ science/science/KenzaView.swift
@@ -0,0 +1,18 @@
+//
+//  KenzaView.swift
+//  science
+//
+//  Created by apprenant91 on 08/03/2024.
+//
+
+import SwiftUI
+
+struct KenzaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KenzaView()
+}
