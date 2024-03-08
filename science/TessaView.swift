58284a1666d17b7a86034bfad1499f812c095152
Oshimaxx <162561467+Oshimaxx@users.noreply.github.com>
2024-03-08 09:45:40 +0100

    tessa1

---
+++ science/TessaView.swift
@@ -0,0 +1,18 @@
+//
+//  TessaView.swift
+//  science
+//
+//  Created by Apprenant68 on 08/03/2024.
+//
+
+import SwiftUI
+
+struct TessaView: View {
+    var body: some View {
+        Text("Hello, coucou c tessa")
+    }
+}
+
+#Preview {
+    TessaView()
+}
